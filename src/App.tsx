fc97aa19d17e2795d998aa4058863a76581a6196
CastilhoY <andradeya4@gmail.com>
2025-10-03 08:25:40 -0300

    feat: utilizando default values, trabalhando com diferentes inputs

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 
 interface FormData {
   nome: string;
-  email: string;
-  senha: string;
+  sexo: string;
+  pais: string;
+  termos: boolean;
 }
 
 function App() {
@@ -12,7 +13,14 @@ function App() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({
+    defaultValues: {
+      nome: "",
+      sexo: "Masculino",
+      pais: "brasil",
+      termos: false,
+    }
+  });
 
   const enviarDados = (data: FormData) => {
     console.log(data);
@@ -24,35 +32,41 @@ function App() {
         <input
           type="text"
           placeholder="Nome"
+          className="inputText"
           {...register("nome", { required: "O nome é Obrigatório" })}
         />
-        {errors.nome && <p style={{color: "red"}}>{errors.nome.message}</p>}
+        {errors.nome && <p style={{ color: "red" }}>{errors.nome.message}</p>}
 
-        <input
-          type="email"
-          placeholder="Email"
-          {...register("email", {
-            required: "O email é Obrigatório",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Formato de Email Inválido",
-            },
-          })}
-        />
-        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
+        <div className="radio-group">
+          <input
+            type="radio"
+            id="masculino"
+            {...register("sexo")}
+            value="Masculino"
+          />
+          <label htmlFor="masculino">Masculino</label>
+        </div>
 
-        <input
-          type="password"
-          placeholder="Senha"
-          {...register("senha", {
-            required: "A senha é obrigatória",
-            minLength: {
-              value: 6,
-              message: "Mínimo de 6 Caracteres",
-            },
-          })}
-        />
-        {errors.senha && <p style={{ color: "red" }}>{errors.senha.message}</p>}
+        <div className="radio-group">
+          <input
+            type="radio"
+            id="feminino"
+            {...register("sexo")}
+            value="Feminino"
+          />
+          <label htmlFor="feminino">Feminino</label>
+        </div>
+
+        <select className="radio-group" {...register("pais")}>
+          <option value="brasil">Brasil</option>
+          <option value="portugal">Portugal</option>
+          <option value="espanha">Espanha</option>
+        </select>
+
+        <div className="radio-group">
+          <input type="checkbox" id="termo" {...register("termos")} />
+          <label htmlFor="termo">Li e aceito os termos de contrato.</label>
+        </div>
 
         <button type="submit">Enviar</button>
       </form>
