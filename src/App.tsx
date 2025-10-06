8cf4e5a273e6cc865540e473b833f9e61226cfcf
CastilhoY <andradeya4@gmail.com>
2025-10-06 14:38:39 -0300

    feat: entendendo o uso do controller

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 import "./App.css";
 
 interface FormData {
   nome: string;
   email: string;
-  senha: string;
-  sexo: string;
-  pais: string;
-  termos: boolean;
+  data: Date | null;
 }
 
 function App() {
   const {
     register,
-    handleSubmit,
-    reset,
-    getValues,
-    setValue,
+    control,
     formState: { errors },
+    handleSubmit,
   } = useForm<FormData>({
     defaultValues: {
       nome: "",
       email: "",
-      senha: "",
-      sexo: "Masculino",
-      pais: "Brasil",
-      termos: false,
+      data: null,
     },
   });
 
@@ -33,97 +27,41 @@ function App() {
     console.log(data);
   };
 
-  function resetar() {
-    return reset();
-  }
-
-  function preencher() {
-    setValue("nome", "Yãry");
-    setValue("sexo", "Masculino");
-    setValue("pais", "Brasil");
-    setValue("termos", true);
-  }
-
-  function mostrarValores() {
-    const valores = getValues();
-    console.log(valores);
-  }
-
   return (
     <>
       <form className="form" onSubmit={handleSubmit(enviarDados)}>
-        <input
+        
+        <input 
+          {...register("nome", { required: "Nome obrigatorio" })}
           type="text"
           placeholder="Nome"
           className="inputText"
-          {...register("nome", { required: "O nome é Obrigatório" })}
         />
-        {errors.nome && <p style={{ color: "red" }}>{errors.nome.message}</p>}
+        <p>{errors.nome?.message}</p>
 
-        <input
+        <input 
+          {...register("email")}
           type="email"
           placeholder="Email"
           className="inputText"
-          {...register("email", {
-            required: "O email é Obrigatório",
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Email Inválido",
-            },
-          })}
         />
-        {errors.email && <p style={{ color: "red" }}>{errors.email.message}</p>}
 
-        <input
-          type="password"
-          placeholder="Senha"
-          className="inputText"
-          {...register("senha", {
-            required: "A Senha é Obrigatória",
-            minLength: {
-              value: 6,
-              message: "Mínimo 6 Caracteres"
-            }
-          })}
+        <Controller
+          name="data"
+          control={control}
+          render={({ field }) => (
+            <DatePicker 
+              placeholderText="Selecione uma data"
+              onChange={(date) => field.onChange(date)}
+              selected={field.value}
+              dateFormat="dd/MM/yyyy"
+              className="inputText"
+            />
+          )}
         />
-        {errors.senha && <p style={{ color: "red" }}>{errors.senha.message}</p>}
-
-        <div className="radio-group">
-          <input
-            type="radio"
-            id="masculino"
-            {...register("sexo")}
-            value="Masculino"
-          />
-          <label htmlFor="masculino">Masculino</label>
-        </div>
-
-        <div className="radio-group">
-          <input
-            type="radio"
-            id="feminino"
-            {...register("sexo")}
-            value="Feminino"
-          />
-          <label htmlFor="feminino">Feminino</label>
-        </div>
-
-        <select className="radio-group" {...register("pais")}>
-          <option value="Brasil">Brasil</option>
-          <option value="Portugal">Portugal</option>
-          <option value="Espanha">Espanha</option>
-        </select>
-
-        <div className="radio-group">
-          <input type="checkbox" id="termo" {...register("termos")} />
-          <label htmlFor="termo">Li e aceito os termos de contrato.</label>
-        </div>
 
         <button type="submit">Enviar</button>
       </form>
-      <button onClick={resetar}>resetar</button>
-      <button onClick={preencher}>preencher</button>
-      <button onClick={mostrarValores}>mostrar valores</button>
     </>
   );
 }
