8b6111dc9fd798a4ce4aef628b621758015d20f3
CastilhoY <andradeya4@gmail.com>
2025-10-07 08:45:12 -0300

    integrando zod ao formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,51 @@
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import "./App.css";
 
-interface FormData {
-  nome: string;
-  email: string;
-  data: Date | null;
-}
+const schema = z.object({
+  nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+  email: z.string().toLowerCase().pipe(z.email("Formato de email inválido")),
+});
+
+type FormData = z.infer<typeof schema>;
 
 function App() {
   const {
     register,
-    control,
     formState: { errors },
     handleSubmit,
   } = useForm<FormData>({
+    resolver: zodResolver(schema),
     defaultValues: {
       nome: "",
       email: "",
-      data: null,
     },
   });
 
   const enviarDados = (data: FormData) => {
-    console.log(data);
+    console.log("Dados enviados:", data);
   };
 
   return (
     <>
       <form className="form" onSubmit={handleSubmit(enviarDados)}>
-        
-        <input 
-          {...register("nome", { required: "Nome obrigatorio" })}
+        <input
+          {...register("nome")}
           type="text"
           placeholder="Nome"
           className="inputText"
         />
         <p>{errors.nome?.message}</p>
 
-        <input 
+        <input
           {...register("email")}
           type="email"
           placeholder="Email"
           className="inputText"
         />
-
-        <Controller
-          name="data"
-          control={control}
-          render={({ field }) => (
-            <DatePicker 
-              placeholderText="Selecione uma data"
-              onChange={(date) => field.onChange(date)}
-              selected={field.value}
-              dateFormat="dd/MM/yyyy"
-              className="inputText"
-            />
-          )}
-        />
+        <p>{errors.email?.message}</p>
 
         <button type="submit">Enviar</button>
       </form>
