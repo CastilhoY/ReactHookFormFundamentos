1120a0bf201cf84340eb07870e0e9fa2d0ad6a35
CastilhoY <andradeya4@gmail.com>
2025-10-08 08:32:16 -0300

    aplicando regex no zod, aplicando confirmação de senha

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,28 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import "./App.css";
 
-const schema = z.object({
-  nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-  email: z.string().toLowerCase().pipe(z.email("Formato de email inválido")),
-});
+const schema = z
+  .object({
+    nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+    email: z
+      .string()
+      .toLowerCase()
+      .pipe(
+        z.email("Formato de email inválido, ex. de email válido: nome@mail.com")
+      ),
+    senha: z
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres")
+      .regex(/^[A-Za-z0-9]+$/, "a senha deve conter apenas letras e números"),
+    ConfirmarSenha: z
+      .string()
+      .min(6, "A senha deve ter pelo menos 6 caracteres"),
+  })
+
+  .refine((data) => data.senha === data.ConfirmarSenha, {
+    message: "As senhas não conferem",
+    path: ["ConfirmarSenha"],
+  });
 
 type FormData = z.infer<typeof schema>;
 
@@ -46,6 +64,22 @@ function App() {
           className="inputText"
         />
         <p>{errors.email?.message}</p>
+
+        <input
+          {...register("senha")}
+          type="password"
+          placeholder="Senha"
+          className="inputText"
+        />
+        <p>{errors.senha?.message}</p>
+
+        <input
+          {...register("ConfirmarSenha")}
+          type="password"
+          placeholder="Confirmar senha"
+          className="inputText"
+        />
+        <p>{errors.ConfirmarSenha?.message}</p>
 
         <button type="submit">Enviar</button>
       </form>
