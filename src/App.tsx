008afea6795d8a14c1462e17181e1ac29eabf36e
CastilhoY <andradeya4@gmail.com>
2025-10-10 14:48:57 -0300

    feat: aprendendo a usar o usefieldarray

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import "react-datepicker/dist/react-datepicker.css";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import "./App.css";
 
-const schema = z
-  .object({
-    nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
-    email: z
-      .string()
-      .toLowerCase()
-      .pipe(
-        z.email("Formato de email inválido, ex. de email válido: nome@mail.com")
-      ),
-    senha: z
-      .string()
-      .min(6, "A senha deve ter pelo menos 6 caracteres")
-      .regex(/^[A-Za-z0-9]+$/, "a senha deve conter apenas letras e números"),
-    ConfirmarSenha: z
-      .string()
-      .min(6, "A senha deve ter pelo menos 6 caracteres"),
-  })
-
-  .refine((data) => data.senha === data.ConfirmarSenha, {
-    message: "As senhas não conferem",
-    path: ["ConfirmarSenha"],
-  });
+const schema = z.object({
+  nome: z.string().min(3, "O nome deve ter pelo menos 3 caracteres"),
+  produtos: z.array(
+    z.object({
+      nome: z
+        .string()
+        .min(3, "O nome do produto deve ter pelo menos 3 caracteres"),
+      quantidade: z.number().min(1, "A quantidade deve ser no mínimo 1"),
+    })
+  ),
+});
 
 type FormData = z.infer<typeof schema>;
 
 function App() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<FormData>({
+  const { control, handleSubmit, register } = useForm<FormData>({
     resolver: zodResolver(schema),
     defaultValues: {
       nome: "",
-      email: "",
+      produtos: [{ nome: "", quantidade: 0 }],
     },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "produtos",
   });
 
   const enviarDados = (data: FormData) => {
@@ -49,39 +39,30 @@ function App() {
   return (
     <>
       <form className="form" onSubmit={handleSubmit(enviarDados)}>
-        <input
-          {...register("nome")}
-          type="text"
-          placeholder="Nome"
-          className="inputText"
-        />
-        <p>{errors.nome?.message}</p>
+        {fields.map((field, index) => (
+          <div key={field.id}>
+            <input placeholder="Produto" {...register(`produtos.${index}.nome`)} />
+            <input
+              type="number"
+              {...register(`produtos.${index}.quantidade`, {
+                valueAsNumber: true,
+              })}
+            />
+            <button type="button" onClick={() => remove(index)}>
+              Remover
+            </button>
+          </div>
+        ))}
 
-        <input
-          {...register("email")}
-          type="email"
-          placeholder="Email"
-          className="inputText"
-        />
-        <p>{errors.email?.message}</p>
-
-        <input
-          {...register("senha")}
-          type="password"
-          placeholder="Senha"
-          className="inputText"
-        />
-        <p>{errors.senha?.message}</p>
-
-        <input
-          {...register("ConfirmarSenha")}
-          type="password"
-          placeholder="Confirmar senha"
-          className="inputText"
-        />
-        <p>{errors.ConfirmarSenha?.message}</p>
-
-        <button type="submit">Enviar</button>
+        <button
+          type="button"
+          onClick={() => append({
+            nome: "",
+            quantidade: 1,
+          })}
+        >
+          Adicionar Produto
+        </button>
       </form>
     </>
   );
